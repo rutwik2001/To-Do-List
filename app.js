4678548502add74f64d9d185b2cf2c3aa1c4c86a
Pasumarthi Rutwik <62496029+rutwik2001@users.noreply.github.com>
2021-03-26 12:34:09 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,114 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const itemsSchema = {
+  name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todo list."
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add a new item."
+});
+
+
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+});
+const defaultArray = [item1, item2, item3];
+
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+}
+
+const List = mongoose.model("list", listSchema);
+
+
+
+
+
+
+app.get("/", function(req, res) {
+
+  Item.find({},function(err, foundItems){
+    if(foundItems.length === 0){
+      Item.insertMany(defaultArray, function(err){
+        if(err){
+          console.log(err)
+        } else{
+          console.log("Successfully saved to the daefault database");
+        }
+      });
+      res.redirect("/");
+    } else{
+        res.render('list', {listTitle: "Today", newListItems: foundItems});
+    }
+
+  })
+
+})
+
+app.post("/", function(req, res){
+  let itemName = req.body.newItem;
+
+  const item = new Item({
+    name: itemName
+  });
+
+  item.save();
+
+  res.redirect("/")
+
+})
+
+app.post("/delete", function(req, res){
+  const checkedItemId = req.body.checkbox;
+  Item.findByIdAndRemove(checkedItemId, function(err){
+    if(err){
+      console.log(err);
+    }else{
+      console.log("Succesfully deleted the item");
+      res.redirect("/");
+    }
+  })
+});
+
+app.get("/:customListName", function(req, res){
+  const customListName = req.params.customListName
+
+  const list = new List({
+    name: customListName,
+    items: defaultArray
+  })
+  list.save();
+
+})
+
+app.post("/work", function(req, res){
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+})
+
+app.listen(8888, function() {
+  console.log("Server started on port 8888");
+})
